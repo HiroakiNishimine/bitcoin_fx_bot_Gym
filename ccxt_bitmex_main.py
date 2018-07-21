b6fb93214f8c53fbaf6cfbe453383dc8edbbe1ce
HiroakiNishimine <hiroaki.nishimine@gmail.com>
2018-07-21 12:27:42 +0900

    出金機能を追加

--- ccxt_bitmex_main.py
+++ ccxt_bitmex_main.py
@@ -51,7 +51,7 @@ if args.lstm:
     model.add(Activation('linear'))
     print('load model...')
     model.load_weights(
-        'weights/dqn_lstm_ccxt_bitmex-v0_weights_2018_7_17_11_39.h5f')
+        '/home/farmhouse/bitmex/bitcoin_fx_bot_fortestNet/weights/dqn_lstm_ccxt_bitmex-v0_weights_2018_7_20_11_42.h5f')
 if args.mlp:
     model = Sequential()
     model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
